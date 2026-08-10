5edb1e488bf9b8c226249d7fb1e16d07d01ba0db
surajgupta <suraj666189@gmail.com>
2026-08-10 23:00:55 +0530

    add removeFromCart, Increase and Decrease route

--- Backend/src/controllers/cart.controller.js
+++ Backend/src/controllers/cart.controller.js
@@ -61,4 +61,73 @@ const getCart = async (req, res) => {
   }
 }
 
-module.exports = { addToCart, getCart }
+const removeFromCart = async (req, res) => {
+  const { productId } = req.params
+
+  try {
+    const user = await userModel.findById(req.user.id)
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+    user.cart = user.cart.filter(item => item.product.toString() !== productId)
+    await user.save()
+    res
+      .status(200)
+      .json({ message: 'Remove From Cart Successfull', cart: user.cart })
+  } catch (error) {
+    res.status(500).json({ message: 'Get Cart Failed', error: error.message })
+  }
+}
+
+const updateCartQuantity = async (req, res) => {
+  const { productId, action } = req.params
+  // const { action } = req.body
+
+  try {
+    const user = await userModel.findById(req.user.id)
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found'
+      })
+    }
+
+    const cartItem = user.cart.find(
+      item => item.product.toString() === productId
+    )
+
+    if (!cartItem) {
+      return res.status(404).json({
+        message: 'Product not found in cart'
+      })
+    }
+
+    if (action === 'increase') {
+      cartItem.quantity += 1
+    }
+
+    if (action === 'decrease') {
+      cartItem.quantity -= 1
+    }
+
+    // quantity 1 se kam nahi hone dena
+    if (cartItem.quantity < 1) {
+      cartItem.quantity = 1
+    }
+
+    await user.save()
+
+    return res.status(200).json({
+      message: 'Cart quantity updated successfully',
+      cart: user.cart
+    })
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Update quantity failed',
+      error: error.message
+    })
+  }
+}
+
+module.exports = { addToCart, getCart, removeFromCart, updateCartQuantity }
--- Backend/src/routes/cart.route.js
+++ Backend/src/routes/cart.route.js
@@ -5,5 +5,14 @@ const authMiddleware = require('../middlewares/auth-middleware')
 
 cartRouter.post('/addtocart', authMiddleware, cartController.addToCart)
 cartRouter.get('/getcart', authMiddleware, cartController.getCart)
-
+cartRouter.delete(
+  '/removefromcart/:productId',
+  authMiddleware,
+  cartController.removeFromCart
+)
+cartRouter.patch(
+  '/updatecartquantity/:productId/:action',
+  authMiddleware,
+  cartController.updateCartQuantity
+)
 module.exports = cartRouter
