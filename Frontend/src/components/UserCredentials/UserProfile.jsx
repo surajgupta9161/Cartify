2e713c8ead8934ef9b3e33c4b075cdfacd6dca40
surajgupta <suraj666189@gmail.com>
2026-08-21 17:58:27 +0530

    edit  back button in profile

--- Frontend/src/components/UserCredentials/UserProfile.jsx
+++ Frontend/src/components/UserCredentials/UserProfile.jsx
@@ -40,20 +40,12 @@ const UserProfile = () => {
   }
 
   return (
-    <div className='min-h-screen py-8'>
-      <div className='grid grid-cols-1 lg:grid-cols-[320px_1fr] gap-6 items-start'>
+    <div className='min-h-screen py-8 relative'>
+      <div className='grid grid-cols-1 lg:grid-cols-[320px_1fr] gap-6 items-start '>
         {/* LEFT SIDE */}
         <div className='bg-[#2d2c2c] border border-gray-700 rounded-2xl p-6 h-fit lg:sticky lg:top-6'>
           {/* PROFILE */}
-          <div className='flex flex-col items-center border-b border-gray-700 pb-6 realative'>
-            <button
-              onClick={() => navigate('/')}
-              className='flex absolute top-5 left-2 items-center gap-2 px-2 py-2 rounded-xl
-             bg-[#333232] border border-gray-700 hover:bg-[#3a3939]
-             transition cursor-pointer font-medium'
-            >
-              <ArrowLeft size={19} />
-            </button>
+          <div className='flex flex-col items-center border-b border-gray-700 pb-6 '>
             <div className='w-20 h-20 rounded-full bg-blue-600 flex items-center justify-center mb-4'>
               <User size={38} />
             </div>
@@ -103,6 +95,14 @@ const UserProfile = () => {
               Logout
             </button>
           </div>
+          <button
+            onClick={() => navigate('/')}
+            className='flex absolute top-10 left-2 items-center gap-2 px-2 py-2 rounded-xl
+             bg-[#333232] border border-gray-700 hover:bg-[#3a3939]
+             transition cursor-pointer font-medium'
+          >
+            <ArrowLeft size={19} />
+          </button>
         </div>
 
         {/* RIGHT SIDE */}
