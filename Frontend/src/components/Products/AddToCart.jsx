9fe0ce961b4aaef16d7f8fe584a8346ca431ba3b
surajgupta <suraj666189@gmail.com>
2026-08-25 18:09:51 +0530

    add toggle to addToCart button

--- Frontend/src/components/Products/AddToCart.jsx
+++ Frontend/src/components/Products/AddToCart.jsx
@@ -1,53 +1,70 @@
 import axios from 'axios'
 import { Check } from 'lucide-react'
+import { useState } from 'react'
 import { useCart } from '../../context/CartContext'
 import { useUser } from '../../context/UserContext'
 
 const AddToCart = ({ productId }) => {
   const { cart, fetchCart } = useCart()
   const { fetchUser } = useUser()
+  const [loading, setLoading] = useState(false)
 
-  // Check product already cart me hai ya nahi
   const isAdded = cart?.some(item => item.product?._id === productId)
 
-  const addToCart = async () => {
-    // Already added hai to request mat bhejo
-    if (isAdded) return
+  const handleCart = async () => {
+    if (loading) return
 
     try {
-      const response = await axios.post(
-        'http://localhost:3000/api/cart/addtocart',
-        { productId },
-        { withCredentials: true }
-      )
+      setLoading(true)
 
-      if (response.status === 200) {
-        await fetchCart()
-        await fetchUser()
+      if (isAdded) {
+        await axios.delete(
+          `http://localhost:3000/api/cart/removefromcart/${productId}`,
+          {
+            withCredentials: true
+          }
+        )
+      } else {
+        await axios.post(
+          'http://localhost:3000/api/cart/addtocart',
+          { productId },
+          {
+            withCredentials: true
+          }
+        )
       }
+      await fetchUser()
+      await fetchCart()
     } catch (error) {
       console.log(error)
+    } finally {
+      setLoading(false)
     }
   }
 
   return (
     <button
-      onClick={addToCart}
-      disabled={isAdded}
+      onClick={handleCart}
+      disabled={loading}
       className={`
         absolute bottom-2 right-2
         rounded-lg font-semibold
         transition active:scale-95
+
         ${
           isAdded
-            ? 'bg-green-600 text-white border border-green-600 px-3 py-1 text-sm cursor-default'
+            ? 'bg-green-600 text-white px-3 py-1 text-sm cursor-pointer hover:bg-red-600'
             : 'bg-white text-pink-600 border border-pink-600 px-5 py-1.5 hover:bg-pink-50 cursor-pointer'
-        } 
-    `}
+        }
+
+        ${loading ? 'opacity-60 cursor-not-allowed' : ''}
+      `}
     >
-      {isAdded ? (
+      {loading ? (
+        '...'
+      ) : isAdded ? (
         <span className='flex items-center gap-1'>
-          <Check size={16} />
+          <Check size={14} />
           Added
         </span>
       ) : (
