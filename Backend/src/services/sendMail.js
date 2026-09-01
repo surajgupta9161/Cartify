3c8ef97ca00640fd901aafde137dc6ac4f6db0c7
surajgupta <suraj666189@gmail.com>
2026-09-01 12:07:51 +0530

    add service mail

--- Backend/src/services/sendMail.js
+++ Backend/src/services/sendMail.js
@@ -1,45 +1,19 @@
-// const nodemailer = require('nodemailer')
-
-// const sendMail = async (to, subject, text) => {
-//   try {
-//     const transporter = nodemailer.createTransport({
-//       service: 'gmail',
-//       auth: {
-//         user: process.env.EMAIL,
-//         pass: process.env.PASSWORD
-//       }
-//     })
-
-//     await transporter.sendMail({
-//       from: `${process.env.EMAIL} from Cartify `,
-//       to: to,
-//       subject: subject,
-//       text: text
-//     })
-//   } catch (error) {
-//     throw new Error(error.message)
-//   }
-// }
-
-// module.exports = { sendMail }
-
 const nodemailer = require('nodemailer')
 
 const sendMail = async (to, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL,
         pass: process.env.PASSWORD
-      }
+      },
+      connectionTimeout: 10000,
+      greetingTimeout: 10000,
+      socketTimeout: 10000
     })
-
-    console.log('EMAIL exists:', !!process.env.EMAIL)
-    console.log('PASSWORD exists:', !!process.env.PASSWORD)
-
-    await transporter.verify()
-    console.log('SMTP connection verified')
 
     const info = await transporter.sendMail({
       from: `Cartify <${process.env.EMAIL}>`,
@@ -49,12 +23,9 @@ const sendMail = async (to, subject, text) => {
     })
 
     console.log('MAIL SENT:', info.messageId)
-    return info
   } catch (error) {
     console.log('MAIL ERROR MESSAGE:', error.message)
     console.log('MAIL ERROR CODE:', error.code)
-    console.log('MAIL ERROR RESPONSE:', error.response)
-
     throw error
   }
 }
